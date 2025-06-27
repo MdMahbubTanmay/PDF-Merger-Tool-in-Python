9462157b0a04bf9ca0166efff5db755f9fba026e
Md Mahbub Tanmay <129056181+MdMahbubTanmay@users.noreply.github.com>
2025-06-27 20:12:19 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import os
-from PyPDF2 import PdfMerger
+import os #For folder operation
+from PyPDF2 import PdfMerger #For Pdf Operation
 filePath= "F:/DeveloperTools/Python Learning/PDFS"
 
 PdfList = []
 for i in os.listdir(filePath):
     if i.endswith(".pdf"):
-        PdfList.append(i)
-PdfList.sort()
-print(PdfList)
+        PdfList.append(i) #Joining in PdfList list
+PdfList.sort() #To Decorate List
+
 PdfHandel = PdfMerger()
 for i in PdfList:
     PdfHandel.append(f"{filePath}/{i}")
